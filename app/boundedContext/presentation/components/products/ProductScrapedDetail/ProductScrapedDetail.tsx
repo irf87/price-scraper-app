d42fac2ca00885b2eb464f5315020e1d3117126a
irf87 <irf87@hotmail.com>
2024-08-28 18:38:46 -0600

    feat: add chart prototype

--- app/boundedContext/presentation/components/products/ProductScrapedDetail/ProductScrapedDetail.tsx
+++ app/boundedContext/presentation/components/products/ProductScrapedDetail/ProductScrapedDetail.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Image, Linking} from 'react-native';
+import {View, Image, Linking, ScrollView} from 'react-native';
 import {Text, Button} from 'react-native-paper';
 
 import {ProductScrapedFormated} from '@application/products/useProducts';
@@ -14,47 +14,52 @@ interface Props {
 
 function ProductScrapedDetail({productDetail, productScrapedRecord}: Props) {
   return (
-    <View>
-      <View style={style.descriptionContainter}>
-        <Image style={style.img} source={{uri: productDetail?.urlImg || ''}} />
-        <Text variant="bodyMedium" style={style.description}>
-          {productDetail?.description}
+    <ScrollView>
+      <View>
+        <View style={style.descriptionContainter}>
+          <Image
+            style={style.img}
+            source={{uri: productDetail?.urlImg || ''}}
+          />
+          <Text variant="bodyMedium" style={style.description}>
+            {productDetail?.description}
+          </Text>
+        </View>
+        <Text variant="bodySmall">
+          Última actualización: {productDetail?.date}
         </Text>
-      </View>
-      <Text variant="bodySmall">
-        Última actualización: {productDetail?.date}
-      </Text>
-      <View style={style.currentPriceSection}>
-        <Text variant="titleSmall" style={style.currentPrice}>
-          Precio actual:
-        </Text>
-        <Text variant="titleSmall">{productDetail.price}</Text>
-      </View>
-      <View style={style.priceBehaviourSection}>
-        <View>
-          <Text variant="bodySmall">Precio Minimo</Text>
-          <Text variant="bodySmall">{productScrapedRecord?.minPrice}</Text>
+        <View style={style.currentPriceSection}>
+          <Text variant="titleSmall" style={style.currentPrice}>
+            Precio actual:
+          </Text>
+          <Text variant="titleSmall">{productDetail.price}</Text>
         </View>
-        <View>
-          <Text variant="bodySmall">Precio Máximo</Text>
-          <Text variant="bodySmall">{productScrapedRecord?.maxPrice}</Text>
+        <View style={style.priceBehaviourSection}>
+          <View>
+            <Text variant="bodySmall">Precio Minimo</Text>
+            <Text variant="bodySmall">{productScrapedRecord?.minPrice}</Text>
+          </View>
+          <View>
+            <Text variant="bodySmall">Precio Máximo</Text>
+            <Text variant="bodySmall">{productScrapedRecord?.maxPrice}</Text>
+          </View>
+          <View>
+            <Text variant="bodySmall">Precio promedio</Text>
+            <Text variant="bodySmall">{productScrapedRecord?.avgPrice}</Text>
+          </View>
         </View>
-        <View>
-          <Text variant="bodySmall">Precio promedio</Text>
-          <Text variant="bodySmall">{productScrapedRecord?.avgPrice}</Text>
+        <View style={style.buttonSection}>
+          <Text variant="bodySmall">{productDetail?.urlScrapedDomainName}</Text>
         </View>
+        <Button
+          mode="contained"
+          onPress={() => {
+            Linking.openURL(productDetail?.urlToScrape);
+          }}>
+          Abrir en el navegador
+        </Button>
       </View>
-      <View style={style.buttonSection}>
-        <Text variant="bodySmall">{productDetail?.urlScrapedDomainName}</Text>
-      </View>
-      <Button
-        mode="contained"
-        onPress={() => {
-          Linking.openURL(productDetail?.urlToScrape);
-        }}>
-        Abrir en el navegador
-      </Button>
-    </View>
+    </ScrollView>
   );
 }
 
